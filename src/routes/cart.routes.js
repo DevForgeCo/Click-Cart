7caebc7a8386ac33037d48e2122cb9a05fe0d5e3
faizan162 <faizanbajwa653@gmail.com>
2025-02-26 01:48:12 +0500

    fixed api routes for cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -11,8 +11,8 @@ import {
 const router = Router();
 
 router.route("/addItemToCart").post(addItemToCart);
-router.route("/getCartItems").get(getCartItems);
-router.route("/updateCartItem").put(verifyJWT, updateCartItem);
+router.route("/getCartItems/:userId").get(getCartItems);
+router.route("/updateCartItem/:cartItemId").put(verifyJWT, updateCartItem);
 router.route("/deleteCartItem").delete(verifyJWT, deleteCartItem);
 
 export default router;
