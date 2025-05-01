08cba9e47fb06ff01056e197c6f1bfb1b90ff3bb
Dev <devmasterappflutter@gmail.com>
2025-05-01 08:48:04 +0500

    delete api functionality

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import isAdmin from "../middlewares/admin.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   fetchOrdersByUser,
   createOrder,
@@ -7,6 +8,7 @@ import {
   fetchAllOrders,
   updateOrderStatus,
   getMonthlySales,
+  cancelOrder,
 } from "../controllers/order.controllers.js";
 
 const router = Router();
@@ -14,9 +16,10 @@ const router = Router();
 // user ke routes hen
 router.post("/create", createOrder);
 router.get("/user/:userId", fetchOrdersByUser);
+router.patch("/cancel/:orderId/:userId", cancelOrder);
 
 // admin or general routes
-router.get("/all", fetchAllOrders);
+router.get("/all", isAdmin, fetchAllOrders);
 router.delete("/:orderId", isAdmin, deleteOrder);
 router.put("/:orderId/status", isAdmin, updateOrderStatus);
 router.get("/order-sales/monthly", isAdmin, getMonthlySales);
