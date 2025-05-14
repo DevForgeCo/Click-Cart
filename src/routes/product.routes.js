661e6daa5ec2f01bab545df13f7b75f9219947c2
Dev <devmasterappflutter@gmail.com>
2025-05-14 08:26:44 +0500

    load product by clientSide

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,17 +3,19 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import isAdmin from "../middlewares/admin.middleware.js";
 import {
   createProduct,
-  fetchAllProducts,
+  fetchAllProductsByAdmin,
   fetchProductById,
   updateProduct,
   deleteProduct,
   searchProducts,
+  fetchAllProductsByClient,
 } from "../controllers/product.controllers.js";
 
 const router = Router();
 
 router.route("/createProduct").post(isAdmin, createProduct);
-router.route("/fetchAllProducts").get(fetchAllProducts);
+router.route("/fetchAllProductsByAdmin").get(fetchAllProductsByAdmin);
+router.route("/fetchAllProductsByClient").get(fetchAllProductsByClient);
 router.route("/fetchProductById/:id").get(fetchProductById);
 router.route("/updateProduct/:id").put(isAdmin, updateProduct);
 router.route("/deleteProduct/:id").delete(isAdmin, deleteProduct);
